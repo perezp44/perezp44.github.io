d7260d7f4439afaf8b9973b017343fe82429bd3e
perezp <pedro.j.perez@uv.es>
2016-04-01 01:27:10 +0200

    htmluuouyuylll

--- _scripts/Mejorar el blog_tutoriales_html.R
+++ _scripts/Mejorar el blog_tutoriales_html.R
@@ -1,17 +1,19 @@
 Quiero poner una nueva pestaña en el blog donde poner mis tutoriales.
 
-Los quiero subir directamente en HTML: esto se puede hacer. generas el HTML con RStudioy luego le pones el YAML header y la extension .md con Notepad++ y luego lo pones en la carpeta del blog _posts
+Los quiero subir directamente en HTML: esto se puede hacer. generas el HTML con RStudio y luego le pones el YAML header y la extension .md con Notepad++ y luego lo pones en la carpeta del blog _posts
 
 
 Como he visto que esto funciona, entonces he queriod ponerlos en otra pestaña. esto tb lo he conseguido:
-#-las pestanas del blog estan en :   _data/navigation.yml
-La carpeta con el fichero index se llma "Tutoriales" pero en el blog se ve como "HTML"
-el yaml del inex es:
+
+  #-las pestanas del blog estan en :   _data/navigation.yml
+La carpeta con el fichero index se llama "Progres" 
+el yaml del index es:
 ---
-layout: post-index-html-pjp
-title: In progress(**)
-excerpt: "A List of Posts (in progress)"
+layout: progre-index
+title: All Progres
+excerpt: "A List of Progres"
 ---
+  
   
 Asi que para esa pagina he de definir un layout. Los layouts estan en la carpeta  _layouts
 
@@ -21,7 +23,7 @@ Lo que he hecho es duplicar casi el layout de post-index
 
 A los posts en html (mis tutoriales) les pondre un YAML como
 ---
-  layout: postpjp
+  layout: progre
 title: "prueba 55"
 author: Pedro J. Perez
 description: "prueba 2 html"
@@ -31,6 +33,7 @@ comments: true
 ---
   
   
-  
-  Asi que tb he tenido que crae un layout para los postpjp
-  
+  Para mi esta todo bien. 
+
+
+Lo unico que no me cuadra es una file que se llama "archive"  que esta en _templates y que no se que otra file utiliza "archive"
